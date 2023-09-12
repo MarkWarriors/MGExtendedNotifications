36429731c5ac5bbef2c916461cce0445e8abfd66
Marco Guerrieri <marco.guerrieri@kinandcarta.com>
2023-09-12 16:57:34 +0100

    added notification target

---
+++ MGNotificationContentExtension/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  MGNotificationContentExtension
+//
+//  Created by Marco Guerrieri on 12/09/2023.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
