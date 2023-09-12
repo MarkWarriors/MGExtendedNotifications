42f0615193b0ef4656271ed97d3789a1f3d55482
Marco Guerrieri <marco.guerrieri@kinandcarta.com>
2023-09-12 16:54:14 +0100

    First commit

---
+++ MGExtendedNotifications/MGExtendedNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MGExtendedNotificationsApp.swift
+//  MGExtendedNotifications
+//
+//  Created by Marco Guerrieri on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MGExtendedNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
